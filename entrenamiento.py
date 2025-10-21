43cda3112c2c59c3ed618b4d0a15ab69830cbfef
Robert <lopez.reyes.jose.m@gmail.com>
2025-10-20 18:38:11 -0600

    🤕Selección del modelo

--- entrenamiento.py
+++ entrenamiento.py
@@ -5,8 +5,11 @@ González Martínez Silvia
 López Reyes José Roberto
 """
 
-# Parte 2: Limpieza final
+# Parte 2: Limpieza final y entrenamiento del modelo
 import pandas as pd
+from sklearn.ensemble import GradientBoostingRegressor, HistGradientBoostingRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, root_mean_squared_error
+from sklearn.preprocessing import StandardScaler
 
 nombre_archivo_original = 'Data/final_datset_clima.csv'
 
@@ -29,3 +32,50 @@ df_final.to_csv(nombre_archivo_final, index=False)
 
 print(f"\nDataset final guardado como: '{nombre_archivo_final}'")
 print(df_final)
+
+X = df_final.drop('temp', axis=1)
+y = df_final['temp']
+
+print(X)
+print(y)
+
+X_train, X_test = X.iloc[:int(len(X) * 0.8)], X.iloc[int(len(X) * 0.8):]
+y_train, y_test = y.iloc[:int(len(X) * 0.8)], y.iloc[int(len(X) * 0.8):]
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+"""GradientBoosting"""
+
+gb = GradientBoostingRegressor()
+gb.fit(X_train, y_train)
+y_pred = gb.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = root_mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("\nModelo: GradientBoosting")
+print("MAE:", mae)
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("R2:", r2)
+
+"""HistGradientBoosting"""
+
+hgb = HistGradientBoostingRegressor()
+hgb.fit(X_train, y_train)
+y_pred = hgb.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = root_mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("\nModelo: HistGradientBoosting")
+print("MAE:", mae)
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("R2:", r2)
---
+++ modelos.py
@@ -0,0 +1,191 @@
+# Práctica 8: Regresor de Clima
+"""
+Integrantes:
+González Martínez Silvia
+López Reyes José Roberto
+"""
+
+# Parte 3: Selección del mejor modelo
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, HistGradientBoostingRegressor
+from xgboost import XGBRegressor
+from sklearn.linear_model import Ridge, Lasso
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, root_mean_squared_error
+from sklearn.preprocessing import StandardScaler
+
+"""Cargar Dataset"""
+
+ruta = 'Data/super_final_dataset.csv'
+
+df = pd.read_csv(ruta)
+
+print(df.head())
+
+X = df.drop('temp', axis=1)
+y = df['temp']
+
+print(X)
+print(y)
+
+X_train, X_test = X.iloc[:int(len(X) * 0.8)], X.iloc[int(len(X) * 0.8):]
+y_train, y_test = y.iloc[:int(len(X) * 0.8)], y.iloc[int(len(X) * 0.8):]
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+"""RandomForest"""
+
+rf = RandomForestRegressor()
+rf.fit(X_train, y_train)
+y_pred = rf.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = root_mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Modelo: RandomForestRegressor")
+print("MAE:", mae)
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("R2:", r2)
+
+"""GradientBoosting"""
+
+gb = GradientBoostingRegressor()
+gb.fit(X_train, y_train)
+y_pred = gb.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = root_mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Modelo: GradientBoosting")
+print("MAE:", mae)
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("R2:", r2)
+
+"""HistGradientBoosting"""
+
+hgb = HistGradientBoostingRegressor()
+hgb.fit(X_train, y_train)
+y_pred = hgb.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = root_mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Modelo: HistGradientBoosting")
+print("MAE:", mae)
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("R2:", r2)
+
+"""XGBRegressor"""
+
+xgbr = XGBRegressor()
+xgbr.fit(X_train, y_train)
+y_pred = xgbr.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = root_mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Modelo: XGBRegressor")
+print("MAE:", mae)
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("R2:", r2)
+
+"""Ridge"""
+
+rge = Ridge(alpha=0.5, tol=0.001, solver='auto', random_state=42)
+rge.fit(X_train, y_train)
+y_pred = rge.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = root_mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Modelo: Ridge")
+print("MAE:", mae)
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("R2:", r2)
+
+"""Lasso"""
+
+las = Lasso(alpha=0.1, tol=0.001, random_state=42)
+las.fit(X_train, y_train)
+y_pred = las.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = root_mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Modelo: Lasso")
+print("MAE:", mae)
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("R2:", r2)
+
+"""SVR"""
+
+svr = SVR(kernel='rbf', C=1.0, epsilon=0.1)
+svr.fit(X_train, y_train)
+y_pred = svr.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = root_mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Modelo: SVR")
+print("MAE:", mae)
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("R2:", r2)
+
+"""KNeighborsRegressor"""
+
+knn = KNeighborsRegressor()
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = root_mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Modelo: KNeighborsRegressor")
+print("MAE:", mae)
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("R2:", r2)
+
+"""MLP"""
+
+MLP = MLPRegressor(random_state=1, max_iter=2000, tol=0.1)
+MLP.fit(X_train, y_train)
+y_pred = MLP.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = root_mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Modelo: MLPRegressor")
+print("MAE:", mae)
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("R2:", r2)
